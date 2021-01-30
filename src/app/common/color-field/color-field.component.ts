a9ada5d8ef9569b4acb15e7b5deb609b5236c87c
Gabriel Meyer <meyergabriel@live.de>
2021-01-30 09:43:46 +0100

    Update

---
+++ src/app/common/color-field/color-field.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-color-field',
+  templateUrl: './color-field.component.html',
+  styleUrls: ['./color-field.component.scss']
+})
+export class ColorFieldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
